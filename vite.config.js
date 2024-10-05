3472422bfb41cd9dc38fb6842c11ad76873c8815
Danish_Official <danishsidd011@gmail.com>
2024-10-05 22:16:57 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
-})
+  },
+  server: {
+    host: '0.0.0.0',             // Bind to 0.0.0.0 for external access
+    port: process.env.PORT || 5173,  // Use the port provided by Render or fallback to 5173
+  },
+});
