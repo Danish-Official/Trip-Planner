ad414349793ccff104d893b221bb500697592188
Danish_Official <danishsidd011@gmail.com>
2024-10-05 22:05:45 +0530

    first commit

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCFKM6sVaZ6xxgahp-2dLTV9ayeHa8QLx8",
-  authDomain: "trip-planner-e490a.firebaseapp.com",
-  projectId: "trip-planner-e490a",
-  storageBucket: "trip-planner-e490a.appspot.com",
-  messagingSenderId: "921396593958",
-  appId: "1:921396593958:web:b6b9ac717f1b572eda6de7",
-  measurementId: "G-VXBLBMMQJJ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
